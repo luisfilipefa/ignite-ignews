20aa885aa8a808511d41ad93a08afe627f8bc7ba
Luis Filipe Fernandes Almeida <luisfilipe.faw@gmail.com>
2021-03-23 14:34:22 -0300

    - preço do produto dinâmico:   - obtido via chamada a api do stripe usando server side props, e retornando o id do preço daquele determinado produto e seu valor unitário;
    - referência do produto no botão de inscrição:
      - botão de inscrição recebe o id do preço daquele determinado produto;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,9 +26,9 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -28,3 +36,20 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1IYDQqKGeo3wD4nBpnBFW7hj");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+  };
+};
